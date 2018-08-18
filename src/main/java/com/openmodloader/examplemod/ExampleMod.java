6783892b26ae6bf28fcdda95a59183206897d583
InsomniaKitten <admin@designdevils.co.uk>
2018-08-18 19:21:44 +0100

    Further example mod improvements

--- src/main/java/com/openmodloader/examplemod/ExampleMod.java
+++ src/main/java/com/openmodloader/examplemod/ExampleMod.java
@@ -2,16 +2,19 @@ package com.openmodloader.examplemod;
 
 import com.openmodloader.api.event.Event;
 import com.openmodloader.loader.event.LoadEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class ExampleMod {
+public final class ExampleMod {
+    private static final Logger LOGGER = LogManager.getLogger("examplemod");
 
     @Event.Subscribe
-    public void construction(LoadEvent.Construction event) {
-        System.out.println("Example Mod Loading");
+    public void onConstruction(LoadEvent.Construction event) {
+        ExampleMod.LOGGER.info("Example Mod loading");
     }
 
     @Event.Subscribe
-    public void construction(LoadEvent.Finalization event) {
-        System.out.println("Example Mod Loaded");
+    public void onFinalization(LoadEvent.Finalization event) {
+        ExampleMod.LOGGER.info("Example Mod loaded");
     }
 }
