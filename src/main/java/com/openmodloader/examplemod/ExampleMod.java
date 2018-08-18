abd8fb1b7f6f00e0d86c0fbc01d74c688cbf5b03
TheCodedOne <coded@hrznstudio.com>
2018-08-18 17:41:18 +0100

    Better example

--- src/main/java/com/openmodloader/examplemod/ExampleMod.java
+++ src/main/java/com/openmodloader/examplemod/ExampleMod.java
@@ -1,7 +1,17 @@
 package com.openmodloader.examplemod;
 
-import com.openmodloader.loader.OpenModLoader;
+import com.openmodloader.api.event.Event;
+import com.openmodloader.loader.event.LoadEvent;
 
 public class ExampleMod {
 
+    @Event.Subscribe
+    public void construction(LoadEvent.Construction event) {
+        System.out.println("Example Mod Loading");
+    }
+
+    @Event.Subscribe
+    public void construction(LoadEvent.Finalization event) {
+        System.out.println("Example Mod Loaded");
+    }
 }
